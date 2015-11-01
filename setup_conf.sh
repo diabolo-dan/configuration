6eaa39d9fa66a902ff8eb0a7a5a01714bd54ab77
diabolo-dan <diabolo.dan@gmail.com>
2015-11-01 17:38:23 +0000

    setup_conf.sh -- shell command to setup configuration

---
+++ setup_conf.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+mkdir ~/.conf.bak
+#This is non-destructive, but can leave named symlinks in the home folder if done repeatedly
+#Solution, don't do that.
+
+#If file exists, back it up, then symlink to our version
+function setup_conf {
+	find ~ -maxdepth 1 -name .$1 -exec mv -n '{}' ~/.conf.bak/ ';'
+	ln -s $(realpath $1) ~/.$1
+}
+
+setup_conf bashrc
+setup_conf vimrc
+setup_conf vim
+setup_conf xprofile
+
+find ~/.i3 -maxdepth 1 -name config -exec mv -n '{}' ~/.conf.bak/ ';' &&
+ln -s $(realpath i3_config) ~/.i3/config
+
+#Clear out symlinks from .conf.bak
+find ~/.conf.bak -maxdepth 1 -type l -delete
+
+
+
+#find ~ -maxdepth 1 -name .vimrc -exec mv '{}' ~/.conf.bak/ ';'
+#ln -s $(realpath vimrc) ~/.vimrc
+
+#find ~ -maxdepth 1 -name .vim -exec mv  '{}' ~/.conf.bak/  ';'
+#ln -s $(realpath vim) ~/.vim
+
+
+
+
